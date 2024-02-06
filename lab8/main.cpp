5b48cc99e4d72ceeacfc8a2f43923bcd96e8e7b7
Zakaria EL KHAYDER <zelkhayder@gmail.com>
2024-02-06 17:10:22 +0100

    I know my alphabets

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,6 +1,25 @@
 #include "stm32f3xx.h"
 #include "pinAccess.h"
 
+extern "C" void TIM6_DAC1_IRQHandler(void)
+{
+    static char c = 'a';
+
+    USART2->TDR = c;
+
+    if (c == 'z')
+    {
+        c = 'a';
+    }
+    else
+    {
+        c++;
+    }
+
+    // Acknowladge
+    TIM6->SR &= ~TIM_SR_UIF;
+}
+
 void serialSetup()
 {
     // input clock = 64MHz.
@@ -9,6 +28,14 @@ void serialSetup()
     RCC->APB1RSTR |= (RCC_APB1RSTR_USART2RST | RCC_APB1RSTR_TIM6RST);
     RCC->APB1RSTR &= ~(RCC_APB1RSTR_USART2RST | RCC_APB1RSTR_TIM6RST);
 
+    // Configure timer
+    TIM6->CNT = 0;
+    TIM6->SR = 0;
+    TIM6->PSC = 64000 - 1; // 1ms
+    TIM6->ARR = 100 - 1;   // 100ms
+    TIM6->CR1 = 1;
+
+    /*  */
     pinMode(GPIOA, 2, OUTPUT);
     pinAlt(GPIOA, 2, 7);
 
@@ -19,13 +46,15 @@ void serialSetup()
      */
     USART2->BRR = 0x0116;
     USART2->CR1 = USART_CR1_RE | USART_CR1_TE | USART_CR1_UE;
+
+    // enable interrupt
+    TIM6->DIER |= TIM_DIER_UIE;
+    NVIC_EnableIRQ(TIM6_DAC1_IRQn);
 }
 
 int main()
 {
     serialSetup();
-
-    USART2->TDR = 'C';
 
     while (1)
     {
