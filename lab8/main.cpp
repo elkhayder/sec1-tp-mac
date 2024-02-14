eb605958c3cbb52375edeb2be9dc527cf4b1951c
Zakaria EL KHAYDER <zelkhayder@gmail.com>
2024-02-14 08:18:08 +0100

    Blocking `serialPrintChar`

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,25 +1,6 @@
 #include "stm32f3xx.h"
 #include "pinAccess.h"
 
-extern "C" void TIM6_DAC1_IRQHandler(void)
-{
-    static char c = 'a';
-
-    USART2->TDR = c;
-
-    if (c == 'z')
-    {
-        c = 'a';
-    }
-    else
-    {
-        c++;
-    }
-
-    // Acknowladge
-    TIM6->SR &= ~TIM_SR_UIF;
-}
-
 void serialSetup()
 {
     // input clock = 64MHz.
@@ -32,7 +13,7 @@ void serialSetup()
     TIM6->CNT = 0;
     TIM6->SR = 0;
     TIM6->PSC = 64000 - 1; // 1ms
-    TIM6->ARR = 100 - 1;   // 100ms
+    TIM6->ARR = 2000 - 1;  // 100ms
     TIM6->CR1 = 1;
 
     /*  */
@@ -50,6 +31,68 @@ void serialSetup()
     // enable interrupt
     TIM6->DIER |= TIM_DIER_UIE;
     NVIC_EnableIRQ(TIM6_DAC1_IRQn);
+}
+
+void serialPrintChar(char c)
+{
+    USART2->TDR = c;
+
+    while (!(USART2->ISR & USART_ISR_TC))
+        ;
+}
+
+void serialPrintString(char *s)
+{
+    while (*s != '\0')
+        serialPrintChar(*s++);
+}
+
+void serialPrintInt(int n)
+{
+    int idx = 0, len = 0;
+    char buffer[10], reversed[12]; // MAX_INT = 2,147,483,647 (10 chars) + sign char + \0
+
+    if (n < 0)
+    {
+        reversed[idx++] = '-';
+        n = -n; // Remove minus sign
+    }
+
+    do
+    {
+        uint8_t ones = n % 10;
+        n /= 10;
+        buffer[len++] = '0' + ones;
+    } while (n != 0);
+
+    for (int i = len - 1; i >= 0; i--)
+    {
+        reversed[idx++] = buffer[i];
+    }
+    reversed[idx] = 0; // Null terminator
+
+    serialPrintString(reversed);
+}
+
+extern "C" void TIM6_DAC1_IRQHandler(void)
+{
+    char msg[] = "Hello, World!\n";
+    serialPrintString(msg);
+    serialPrintInt(100);
+    serialPrintChar('\n');
+    serialPrintInt(937103);
+    serialPrintChar('\n');
+    serialPrintInt(1094197483);
+    serialPrintChar('\n');
+    serialPrintInt(0);
+    serialPrintChar('\n');
+    serialPrintInt(-1094197483);
+    serialPrintChar('\n');
+    serialPrintInt(-487232425);
+    serialPrintChar('\n');
+
+    // Acknowladge
+    TIM6->SR &= ~TIM_SR_UIF;
 }
 
 int main()
